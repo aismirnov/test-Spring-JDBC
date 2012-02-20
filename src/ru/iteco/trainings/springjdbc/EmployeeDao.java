2b6989f97020c1c71443661f97018471d6d260d1
Alexey Smirnov <av.smirnov@i-teco.ru>
2012-02-20 15:25:32 +0300

    CRUD ready, poor tests passed

--- src/ru/iteco/trainings/springjdbc/EmployeeDao.java
+++ src/ru/iteco/trainings/springjdbc/EmployeeDao.java
@@ -8,6 +8,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 public class EmployeeDao implements EmployeeCRUD {
@@ -15,15 +18,17 @@ public class EmployeeDao implements EmployeeCRUD {
 	public void setTransactionManager(DataSourceTransactionManager transactionManager) {
 		DataSource dataSource = transactionManager.getDataSource();
 	    jdbcTemplate = new JdbcTemplate(dataSource);
+	    namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
 	}
 	
 	public void create(Employee emp) {
-		
+		this.jdbcTemplate.update(sqlQueryToCreateEmp,
+				emp.getNumber(), emp.getName(), emp.getJobTitle());
 	}
 	
 	public Employee retrieve(int empNumber) {
 		Employee emp = this.jdbcTemplate.queryForObject(
-				sqlQueryForRetrieveEmpByNumber,
+				sqlQueryToRetrieveEmp,
 		        new Object[]{empNumber},
 		        new RowMapper<Employee>() {
 		        	public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -38,11 +43,13 @@ public class EmployeeDao implements EmployeeCRUD {
 	}
 	
 	public void update(Employee emp) {
-		
+		this.jdbcTemplate.update(sqlQueryToUpdateEmp,
+				emp.getNumber(), emp.getName(), emp.getJobTitle(), emp.getNumber());
 	}
 	
 	public void delete(Employee emp) {
-		
+		this.jdbcTemplate.update(sqlQueryToDeleteEmp,
+				emp.getNumber());
 	}
 	
 	
@@ -60,22 +67,38 @@ public class EmployeeDao implements EmployeeCRUD {
 				});
 		return employees;
 	}
-
-	public static void main(String argv[]) {
+	
+	public static void main(String[] args) {
 		ApplicationContext context = new FileSystemXmlApplicationContext("src\\context.xml");
-		EmployeeDao ed = (EmployeeDao)context.getBean("employeeDao");
-		List<Employee> empList = ed.getEmployeeList();
-		for(Employee emp: empList) {
-			System.out.println(emp);
-		}
-		System.out.println();
-		System.out.println(ed.retrieve(7369));
+		EmployeeDao empDao = (EmployeeDao)context.getBean("employeeDao");
+		Employee newEmp = new Employee(111, "222", "333");
+		empDao.delete(newEmp);
+		
 	}
+
 	
 	private JdbcTemplate jdbcTemplate;
-	private String sqlQueryForRetrieveEmpByNumber;
+	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 	
-	public void setSqlQueryForRetrieveEmpByNumber(String sqlQuery) {
-		this.sqlQueryForRetrieveEmpByNumber = sqlQuery;
+	private String sqlQueryToCreateEmp;
+	private String sqlQueryToRetrieveEmp;
+	private String sqlQueryToUpdateEmp;
+	private String sqlQueryToDeleteEmp;
+	
+	public void setSqlQueryToCreateEmp(String sqlQueryToCreateEmp) {
+		this.sqlQueryToCreateEmp = sqlQueryToCreateEmp;
 	}
+
+	public void setSqlQueryToRetrieveEmp(String sqlQueryToRetrieveEmp) {
+		this.sqlQueryToRetrieveEmp = sqlQueryToRetrieveEmp;
+	}
+
+	public void setSqlQueryToUpdateEmp(String sqlQueryToUpdateEmp) {
+		this.sqlQueryToUpdateEmp = sqlQueryToUpdateEmp;
+	}
+
+	public void setSqlQueryToDeleteEmp(String sqlQueryToDeleteEmp) {
+		this.sqlQueryToDeleteEmp = sqlQueryToDeleteEmp;
+	}
+	
 }
--- src/ru/iteco/trainings/springjdbc/EmployeeDaoTest.java
+++ src/ru/iteco/trainings/springjdbc/EmployeeDaoTest.java
@@ -1,56 +1,80 @@
 package ru.iteco.trainings.springjdbc;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
+import org.junit.After;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-/*
-@RunWith(SpringJUnit4ClassRunner.class)
+
+//@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 @TransactionConfiguration(transactionManager="transactionManager", defaultRollback=false)
-@Transactional */
+@Transactional
 
 public class EmployeeDaoTest {
-	static ApplicationContext context;
-	static EmployeeDao empDao;
+	private static EmployeeDao empDao;
+	private static Employee emp1, emp2, emp3;
+	private static Employee newEmp;
 	
 	@BeforeClass
 	public static void init() {
-		context = new FileSystemXmlApplicationContext("src\\context.xml");
+		ApplicationContext context = new FileSystemXmlApplicationContext("src\\context.xml");
 		empDao = (EmployeeDao)context.getBean("employeeDao");
+		
+		emp1 = new Employee(7369, "John Smith", "Clerk");
+		emp2 = new Employee(7499, "Joe Allen", "Salesman");
+		emp3 = new Employee(7521, "Mary Lou", "Director");
+		newEmp = new Employee(111, "222", "333");
 	}
 	
 	@Test
-	public void testCreate() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	public void testRetrieve() {
-		
-		Employee emp = new Employee(7369, "John Smith", "Clerk");
-		assertEquals(emp, empDao.retrieve(7369));
-		
-		emp = new Employee(7499, "Joe Allen", "Salesman");
-		assertEquals(emp, empDao.retrieve(7499));
+		assertEquals(emp1, empDao.retrieve(7369));
+		assertEquals(emp2, empDao.retrieve(7499));
+	}
+	
+	@Test
+	@Rollback(true)
+	public void testCreate() {
+		empDao.create(newEmp);
+		assertEquals(newEmp, empDao.retrieve(newEmp.getNumber()));
+	}
+	
+	@Test
+	public void testDelete() {
+		empDao.delete(newEmp);
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testGetEmployeeList() {
+		List<Employee> empList = Arrays.asList(emp1, emp2, emp3);
+		assertEquals(empList, empDao.getEmployeeList());
 	}
 
 	@Test
 	public void testUpdate() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testDelete() {
-		fail("Not yet implemented");
+		emp1.setName("New Name");
+		empDao.update(emp1);
+		assertEquals(emp1, empDao.retrieve(emp1.getNumber()));
+		
+		emp1.setName("John Smith");
+		empDao.update(emp1);
+		assertEquals(emp1, empDao.retrieve(emp1.getNumber()));
 	}
 
 }
